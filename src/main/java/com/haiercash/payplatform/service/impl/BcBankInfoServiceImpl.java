77e4fb58d91f4c576f373a71144fc3522cc853f8
liuqingxiang <liuqingxiang@bestvike.com>
2017-07-27 16:46:05 +0800

    增加eureka调用示例

--- src/main/java/com/haiercash/payplatform/service/impl/BcBankInfoServiceImpl.java
+++ src/main/java/com/haiercash/payplatform/service/impl/BcBankInfoServiceImpl.java
@@ -1,9 +1,13 @@
 package com.haiercash.payplatform.service.impl;
 
+import com.haiercash.commons.rest.EurekaRest;
 import com.haiercash.payplatform.dao.BcBankInfoDao;
+import com.haiercash.payplatform.data.AppOrder;
 import com.haiercash.payplatform.data.BcBankInfo;
 import com.haiercash.payplatform.service.BcBankInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,8 +21,14 @@ public class BcBankInfoServiceImpl implements BcBankInfoService{
     @Autowired
     private BcBankInfoDao bcBankInfoDao;
 
+    @Autowired
+    private EurekaRest eurekaRest;
+
     @Override
     public BcBankInfo selectById(String id) {
+
+        ResponseEntity<AppOrder> responseEntity = eurekaRest.exchange("CRM", "/app/crm/demo", HttpMethod.GET, null, AppOrder.class);
+        AppOrder body = responseEntity.getBody();
 
         return bcBankInfoDao.selectById("123");
     }
