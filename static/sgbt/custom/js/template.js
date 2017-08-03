cf33ac2aa98f0849792dcfc1ff2d510e005d0ea5
lihua <lihua@bestvike.com>
2017-08-03 09:50:33 +0800

    顺逛白条修改

---
+++ static/sgbt/custom/js/template.js
@@ -0,0 +1,43 @@
+require(['vue', 'jquery', 'util'], function(vue, $, util) {
+    vue.component('tab-applyQuota', {
+        props: {
+            active: {
+                default: 'realName'
+            }
+        },
+        data: function () {
+            return {
+                innerTabs: [
+                    {
+                        text: '实名',
+                        clazz: 'realName'
+                    },
+                    {
+                        text: '个人资料',
+                        clazz: 'personal'
+                    },
+                    {
+                        text: '人脸识别',
+                        clazz: 'face'
+                    },
+                    {
+                        text: '密码设置',
+                        clazz: 'password'
+                    }
+                ]
+            }
+        },
+        /****** 模板定义 ******/
+        template: util.heredoc(function() {
+            /*!
+            <div class="tab-applyQuota">
+                <ul class="thumbnail">
+                    <li v-for="el in innerTabs" :class="[{'active': active === el.clazz}, el.clazz]" v-text="el.text"></li>
+                </ul>
+                <div class="clear height10"></div>
+                <div class="height7"></div>
+            </div>
+            */
+        })
+    });
+});
