69a191703e17f9c01599fadf4e4436cb1e3ba733
lihua <lihua@bestvike.com>
2017-08-03 18:17:06 +0800

    增加统一header配置

--- static/sgbt/custom/js/config.js
+++ static/sgbt/custom/js/config.js
@@ -1,7 +1,11 @@
 var __baseResourcePath = '/mobile';
 var __Const = {
     rest: {
-        baseUrl: window.location.protocol + "//" + window.location.host
+        baseUrl: window.location.protocol + "//" + window.location.host,
+        headers: {
+            channel: 'xx',
+            channelNo: 'yy'
+        }
     },
     route: {
         baseLocation: '/sgbt',
