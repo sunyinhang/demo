d92a39ae38d844208f88e0589996cb800ef51f11
zhaoxianlu <zhaoxianlu@bestvike.com>
2017-08-09 17:47:08 +0800

    贷款详情页面:还款总额

--- src/main/java/com/haiercash/payplatform/common/service/impl/FaceServiceImpl.java
+++ src/main/java/com/haiercash/payplatform/common/service/impl/FaceServiceImpl.java
@@ -2,13 +2,15 @@ package com.haiercash.payplatform.common.service.impl;
 
 import com.amazonaws.util.IOUtils;
 import com.haiercash.commons.redis.Cache;
-import com.haiercash.payplatform.common.service.AppServerService;
 import com.haiercash.payplatform.common.service.FaceService;
 import com.haiercash.payplatform.common.utils.ConstUtil;
+import com.haiercash.payplatform.common.utils.DataConverUtil;
 import com.haiercash.payplatform.common.utils.EncryptUtil;
 import com.haiercash.payplatform.common.utils.HttpClient;
+import com.haiercash.payplatform.common.service.AppServerService;
 import com.haiercash.payplatform.service.BaseService;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONObject;
@@ -24,9 +26,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+
+import static com.haiercash.payplatform.common.utils.RestUtil.fail;
+import static com.haiercash.payplatform.common.utils.RestUtil.success;
 
 /**
  * Created by yuanli on 2017/8/1.
