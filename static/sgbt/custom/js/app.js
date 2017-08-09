2c74ef11f93eb92339d03bde6f9ff474a5d1e5a5
程慧梅 <1107653553@qq.com>
2017-08-09 17:43:47 +0800

    页面新增

--- static/sgbt/custom/js/app.js
+++ static/sgbt/custom/js/app.js
@@ -32,11 +32,65 @@ require(['vue', 'jquery', 'util'], function(vue, $, util) {
             /*!
             <div class="tab-applyQuota">
                 <ul>
-                    <li v-for="el in innerTabs" :class="[{'active': active.indexOf(el.clazz) >= 0}, el.clazz]" v-text="el.text"></li>
+                    <li v-for="el in innerTabs" :class="[{'active': active === el.clazz}, el.clazz]" v-text="el.text"></li>
                 </ul>
             </div>
             */
         })
+    });
+    vue.component('quotaState',{
+        template: util.heredoc(function(){
+            /*!
+             <div class="amtSituTopPrnt">
+                <div class="btmBg"><img v-bind:src="imgSrc"/></div>
+                 <div class="cvgCont">
+                     <div class="amtNotTitle"><span>{{title}}</span></div>
+                     <div class="amtNotSubTitle" v-if="whether">{{subTitle}}</div>
+                     <div :class="amtNum">{{num}}</div>
+                     <div class="amtReson" v-if="whetherDesc">您暂不符合申请条件，请稍后再试</div>
+                     <div class="amtActivBtn" v-if="whetherBtn"><a href="javascript:void(0);" :class="activBtn" v-on:click='btnClickFn'>{{btnText}}</a></div>
+                 </div>
+             </div>
+             */
+        }),
+        props:['imgSrc','title','whether','subTitle','amtNum','num','whetherBtn','activBtn','btnText','whetherDesc'],
+        data: function(){
+            return{
+            }
+        },
+        methods:{
+            btnClickFn: function(){
+                this.$emit('on-click');
+            }
+        }
+    });
+    vue.component('staticText',{
+        template: util.heredoc(function(){
+            /*!
+            <div class="b-downloadTips">
+             <template v-for="el in items">
+                 <div class="downloadTips bluefont margt24"><div class="blueCircle"></div><div class="ft">{{el.title}}</div><div class="clear"></div></div>
+                 <div class="downloadTips2 grayfont" v-for="desc in el.descs">{{ desc }}</div>
+             </template>
+            </div>
+             */
+        }),
+        data: function(){
+            return {
+                items: [
+                    {
+                        title: '下载海尔消费金融app“嗨付”',
+                        descs: ['可获得更高额度，可查询审批进度，详细还款计划，查看借款合同，进行主动还款']
+                    },{
+                        title: '下载方式',
+                        descs: ['ios通过app store搜索“嗨付”','安卓通过各大市场搜索“嗨付”']
+                    },{
+                        title: '关注海尔会员中心官微',
+                        descs: ['在个人中心-现金额度中查看额度，并申请贷款']
+                    }
+                ]
+            }
+        },
     });
 });
 var __areaCache = {};
