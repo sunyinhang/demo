28f4871f77e0ff2c6bdc8f7f9d988a00b441b903
lihua <lihua@bestvike.com>
2017-08-11 10:08:40 +0800

    微调

--- src/main/java/com/haiercash/payplatform/common/controller/Democontroller.java
+++ src/main/java/com/haiercash/payplatform/common/controller/Democontroller.java
@@ -1,16 +1,11 @@
 package com.haiercash.payplatform.common.controller;
 
-import com.haiercash.commons.redis.Cache;
-import com.haiercash.commons.rest.inner.InnerResponse;
-import com.haiercash.commons.rest.inner.InnerRestUtil;
-import com.haiercash.payplatform.common.annotation.RequestCheck;
-import com.haiercash.payplatform.common.dao.BcBankInfoDao;
-import com.haiercash.payplatform.common.data.BcBankInfo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import com.haiercash.payplatform.common.utils.ConstUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * demo controller.
@@ -24,27 +19,14 @@ public class Democontroller extends BaseController{
         super("01");
     }
 
-    @Autowired
-    private InnerRestUtil innerRestUtil;
-
-    @Autowired
-    private BcBankInfoDao bcBankInfoDao;
-
-    @Autowired
-    private Cache cache;
-
-
-    @RequestCheck
-    @RequestMapping(value = "/app/payplatform/demo", method = RequestMethod.GET)
-    public InnerResponse demo(String name, Model model) throws Exception {
-        BcBankInfo bcBankInfo = bcBankInfoDao.selectById("468203");
-
-
-        cache.set("XYZ", "12000");
-        String value = cache.get("XYZ");
-
-        return null;
+    // TODO: 测试程序
+    @RequestMapping(value = "/api/demo/allCache", method = RequestMethod.GET)
+    public Map<String, Object> allCache() {
+        String token = httpServletRequest.getHeader("token");
+        if (token == null || "".equals(token)) {
+            logger.info("token为空");
+            return fail(ConstUtil.ERROR_CODE, ConstUtil.TIME_OUT);
+        }
+        return (Map<String, Object>) cache.get(token);
     }
-
-
 }
