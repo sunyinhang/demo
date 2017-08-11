9eba5b039b88c4b46e2c079d33e3b9c6e35a211b
lihua <lihua@bestvike.com>
2017-08-11 12:42:00 +0800

    util库修改，支持redis有效期设置

--- src/main/java/com/haiercash/payplatform/common/controller/Democontroller.java
+++ src/main/java/com/haiercash/payplatform/common/controller/Democontroller.java
@@ -1,6 +1,8 @@
 package com.haiercash.payplatform.common.controller;
 
+import com.haiercash.commons.redis.Cache;
 import com.haiercash.payplatform.common.utils.ConstUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,9 @@ public class Democontroller extends BaseController{
         super("01");
     }
 
+    @Autowired
+    private Cache cache;
+
     // TODO: 测试程序
     @RequestMapping(value = "/api/demo/allCache", method = RequestMethod.GET)
     public Map<String, Object> allCache() {
@@ -28,5 +33,15 @@ public class Democontroller extends BaseController{
             return fail(ConstUtil.ERROR_CODE, ConstUtil.TIME_OUT);
         }
         return (Map<String, Object>) cache.get(token);
+    }
+
+    @RequestMapping(value = "/api/demo/set", method = RequestMethod.GET)
+    public String setCache() {
+        cache.set("demo", "demo123", 5);
+        return cache.get("demo");
+    }
+    @RequestMapping(value = "/api/demo/get", method = RequestMethod.GET)
+    public String getCache() {
+        return cache.get("demo");
     }
 }
