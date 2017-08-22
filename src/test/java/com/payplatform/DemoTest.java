a13fff636f3d00107d7e107772f8fe5715f61e4c
liuqingxiang <liuqingxiang@bestvike.com>
2017-08-22 16:36:45 +0800

    顺逛白条默认不需要商户确认

---
+++ src/test/java/com/payplatform/DemoTest.java
@@ -0,0 +1,11 @@
+package com.payplatform;
+
+import com.payplatform.impl.DemoTestImpl;
+
+/**
+ * demo for unit test.
+ * @author Liu qingxiang
+ * @see DemoTestImpl
+ */
+public interface DemoTest {
+}
