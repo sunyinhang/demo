2de30f7d771991ed676f381a5add17641527ef61
TimAndy <xuchonglei@126.com>
2017-09-07 17:54:24 +0800

    修改为多模块

---
+++ bestvike-core/src/main/java/com/bestvike/lang/CharUtils.java
@@ -0,0 +1,10 @@
+package com.bestvike.lang;
+
+/**
+ * char 扩展
+ *
+ * @author 许崇雷
+ * @date 2017/6/2
+ */
+public class CharUtils extends org.apache.commons.lang3.CharUtils {
+}
---
+++ bestvike-core/src/main/java/com/bestvike/serialization/JsonSerializer.java
@@ -0,0 +1,57 @@
+package com.bestvike.serialization;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+
+import java.util.List;
+
+/**
+ * Created by 许崇雷 on 2016/5/23.
+ * Json 转换类
+ * 字段上加注解控制行为 @JSONField
+ */
+public final class JsonSerializer {
+    /**
+     * 序列化
+     *
+     * @param obj 要序列化的对象
+     * @return 序列化后的json
+     */
+    public static String serialize(Object obj) {
+        return JSON.toJSONString(obj);
+    }
+
+    /**
+     * 反序列化
+     *
+     * @param json  字符串
+     * @param clazz 类型
+     * @return 对象
+     */
+    public static <T> T deserialize(String json, Class<T> clazz) {
+        return JSON.parseObject(json, clazz);
+    }
+
+    /**
+     * 反序列化
+     *
+     * @param json 字符串
+     * @param type 类型引用
+     * @param <T>
+     * @return
+     */
+    public static <T> T deserialize(String json, TypeReference<T> type) {
+        return JSON.parseObject(json, type);
+    }
+
+    /**
+     * 反序列化数组
+     *
+     * @param json  字符串
+     * @param clazz 类型
+     * @return 对象列表
+     */
+    public static <T> List<T> deserializeArray(String json, Class<T> clazz) {
+        return JSON.parseArray(json, clazz);
+    }
+}
