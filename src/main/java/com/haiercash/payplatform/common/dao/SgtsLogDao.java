e67568a20c4d6171a12bc3d381ca85cefec4e527
zhaoxianlu <zhaoxianlu@bestvike.com>
2017-09-14 18:54:02 +0800

    mq推送修改

--- src/main/java/com/haiercash/payplatform/common/dao/SgtsLogDao.java
+++ src/main/java/com/haiercash/payplatform/common/dao/SgtsLogDao.java
@@ -19,4 +19,16 @@ public interface SgtsLogDao extends BaseMapper<SgtsLog>{
  Integer selectcount(String applSeq);
  Integer selectcounts(@Param("applSeq") String applSeq,@Param("outSts") String outSts);
 
+ Integer selectappl(@Param("applSeq") String applSeq,@Param("outSts") String outSts);
+ Integer selectapplq(@Param("applSeq") String applSeq,@Param("outSts") String outSts);
+
+ Integer updateBySeq(@Param("applSeq") String applSeq,@Param("outSts") String outSts);
+
+ Integer selectappltyp(@Param("applSeq") String applSeq,@Param("msgTyp") String msgTyp);
+
+ Integer updateBytyp(@Param("applSeq") String applSeq,@Param("msgTyp") String msgTyp);
+
+
+ Integer selectmsgtyp(@Param("applSeq") String applSeq,@Param("msgTyp") String msgTyp);
+
 }
