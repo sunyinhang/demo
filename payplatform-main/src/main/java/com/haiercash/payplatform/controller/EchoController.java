7181a2300fd9919244f2c88c01f777be367d71fc
TimAndy <xuchonglei@126.com>
2017-09-18 16:24:02 +0800

    添加 jenkins 测试借口

---
+++ payplatform-main/src/main/java/com/haiercash/payplatform/controller/EchoController.java
@@ -0,0 +1,22 @@
+package com.haiercash.payplatform.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * Created by 许崇雷 on 2017-09-18.
+ */
+@RestController
+public class EchoController extends BaseController {
+
+    public EchoController(String moduleNo) {
+        super("00");
+    }
+
+    @GetMapping("/api/echo")
+    public Map<String, Object> echo(String value) {
+        return success(value);
+    }
+}
