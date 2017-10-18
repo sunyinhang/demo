ca6415b8333181eab8f344a146ed9f18e990ccff
TimAndy <xuchonglei@126.com>
2017-10-18 16:06:39 +0800

    日志跟踪 body 最长打印 8KB 字符

--- payplatform-base/src/main/java/com/haiercash/payplatform/ribbon/ClientInputStreamWrapper.java
+++ payplatform-base/src/main/java/com/haiercash/payplatform/ribbon/ClientInputStreamWrapper.java
@@ -12,7 +12,7 @@ import java.nio.charset.Charset;
  * Created by 许崇雷 on 2017-10-16.
  */
 public final class ClientInputStreamWrapper extends InputStream {
-    private static final int MAX_CACHE = 1024 * 128;
+    private static final int MAX_CACHE = 1024 * 8;
     private static final int BUFFER_SIZE = 1024 * 8;
     private static final String BODY_PARSE_FAIL = "内容转换为字符串失败";
     private static final String BODY_HAS_MORE = "(...内容过大，无法显示)";
