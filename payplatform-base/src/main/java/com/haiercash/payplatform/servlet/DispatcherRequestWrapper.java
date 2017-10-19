e7016a5ce7ebeb493fb0bcd12dc9ba55d00ccec8
TimAndy <xuchonglei@126.com>
2017-10-19 14:37:11 +0800

    调用链 ID 改为在线程上下文生成,此处不再生成

--- payplatform-base/src/main/java/com/haiercash/payplatform/servlet/DispatcherRequestWrapper.java
+++ payplatform-base/src/main/java/com/haiercash/payplatform/servlet/DispatcherRequestWrapper.java
@@ -1,13 +1,8 @@
 package com.haiercash.payplatform.servlet;
 
-import com.bestvike.collection.ArrayUtils;
-import com.bestvike.lang.StringUtils;
-import com.haiercash.payplatform.diagnostics.TraceID;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.IOException;
-import java.util.Enumeration;
 
 /**
  * 请求包装器
@@ -16,17 +11,10 @@ import java.util.Enumeration;
  * @date 2017/6/29
  */
 public final class DispatcherRequestWrapper extends HttpServletRequestWrapper {
-    private String traceID;
     private DispatcherInputStreamWrapper inputStream;
 
     public DispatcherRequestWrapper(HttpServletRequest request) {
         super(request);
-        this.init();
-    }
-
-    protected void init() {
-        String value = super.getHeader(TraceID.NAME);
-        this.traceID = StringUtils.isEmpty(value) ? TraceID.generate() : value;
     }
 
     @Override
@@ -34,15 +22,5 @@ public final class DispatcherRequestWrapper extends HttpServletRequestWrapper {
         if (this.inputStream == null)
             this.inputStream = new DispatcherInputStreamWrapper(super.getInputStream());
         return this.inputStream;
-    }
-
-    @Override
-    public String getHeader(String name) {
-        return StringUtils.equalsIgnoreCase(name, TraceID.NAME) ? this.traceID : super.getHeader(name);
-    }
-
-    @Override
-    public Enumeration<String> getHeaders(String name) {
-        return StringUtils.equalsIgnoreCase(name, TraceID.NAME) ? ArrayUtils.asEnumeration(this.traceID) : super.getHeaders(name);
     }
 }
