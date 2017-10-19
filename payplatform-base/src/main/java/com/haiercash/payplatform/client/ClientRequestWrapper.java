8c7661c7daa84938a4d896c357a2ba51aaecb36b
TimAndy <xuchonglei@126.com>
2017-10-19 16:20:13 +0800

    参数重命名

--- payplatform-base/src/main/java/com/haiercash/payplatform/client/ClientRequestWrapper.java
+++ payplatform-base/src/main/java/com/haiercash/payplatform/client/ClientRequestWrapper.java
@@ -12,43 +12,43 @@ import java.net.URI;
  * Created by 许崇雷 on 2017-10-16.
  */
 public final class ClientRequestWrapper extends AbstractClientHttpRequest {
-    private final AbstractClientHttpRequest clientHttpRequest;
+    private final AbstractClientHttpRequest request;
     private ClientOutputStreamWrapper outputStream;
 
-    public ClientRequestWrapper(AbstractClientHttpRequest clientHttpRequest) {
-        this.clientHttpRequest = clientHttpRequest;
+    public ClientRequestWrapper(AbstractClientHttpRequest request) {
+        this.request = request;
     }
 
     @Override
     public HttpMethod getMethod() {
-        return this.clientHttpRequest.getMethod();
+        return this.request.getMethod();
     }
 
     @Override
     public URI getURI() {
-        return this.clientHttpRequest.getURI();
+        return this.request.getURI();
     }
 
     @Override
-    public ClientOutputStreamWrapper getBodyInternal(HttpHeaders httpHeaders) throws IOException {
+    public ClientOutputStreamWrapper getBodyInternal(HttpHeaders headers) throws IOException {
         if (this.outputStream == null)
-            this.outputStream = new ClientOutputStreamWrapper(this.clientHttpRequest.getBody());
+            this.outputStream = new ClientOutputStreamWrapper(this.request.getBody());
         return this.outputStream;
     }
 
     @Override
-    protected ClientResponseWrapper executeInternal(HttpHeaders httpHeaders) throws IOException {
-        this.clientHttpRequest.getHeaders().putAll(httpHeaders);
+    protected ClientResponseWrapper executeInternal(HttpHeaders headers) throws IOException {
+        this.request.getHeaders().putAll(headers);
         StringBuilder builder = OutgoingLog.writeRequestLog(this);
         long begin = System.currentTimeMillis();
-        ClientResponseWrapper responseWrapper;
+        ClientResponseWrapper response;
         try {
-            responseWrapper = new ClientResponseWrapper(this.clientHttpRequest.execute());
+            response = new ClientResponseWrapper(this.request.execute());
         } catch (Exception e) {
             OutgoingLog.writeError(builder, e, System.currentTimeMillis() - begin);
             throw e;
         }
-        OutgoingLog.writeResponseLog(builder, responseWrapper, System.currentTimeMillis() - begin);
-        return responseWrapper;
+        OutgoingLog.writeResponseLog(builder, response, System.currentTimeMillis() - begin);
+        return response;
     }
 }
