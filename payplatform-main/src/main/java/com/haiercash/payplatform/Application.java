c9d5d7f8485f7d858bb1b26b1f69311d12d71d4d
TimAndy <xuchonglei@126.com>
2017-10-20 09:37:34 +0800

    应用程序入口调整

--- payplatform-main/src/main/java/com/haiercash/payplatform/Application.java
+++ payplatform-main/src/main/java/com/haiercash/payplatform/Application.java
@@ -1,15 +1,8 @@
 package com.haiercash.payplatform;
 
-import com.haiercash.commons.properties.RestProfileProperties;
-import com.haiercash.payplatform.config.EurekaServer;
 import com.haiercash.payplatform.client.RestTemplateEx;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -17,7 +10,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -25,20 +18,16 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 import org.springframework.web.client.RestTemplate;
 
 /**
- * 2017-02-28: 增加扫描范围，启用cmis数据源配置
+ * 应用程序入口
  */
-
 @SpringBootApplication
 @EnableRedisHttpSession
-@ServletComponentScan
-@ComponentScan(basePackages = {"com.haiercash.commons", "com.haiercash.payplatform"})
-@EnableEurekaClient
 @EnableZuulProxy
 @EnableScheduling
-@Configuration
-@EnableAutoConfiguration
-@EnableConfigurationProperties({RestProfileProperties.class, EurekaServer.class})
-public class Application extends SpringBootServletInitializer {
+@EnableEurekaClient
+@ComponentScan(basePackages = {"com.haiercash.commons", "com.haiercash.payplatform"},
+        excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.haiercash.commons.config.*"))
+public class Application {
     @Primary
     @LoadBalanced
     @Bean
@@ -54,10 +43,5 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
         context.addApplicationListener((ApplicationListener<ContextClosedEvent>) event -> System.out.println("应用程序正常退出..."));
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
     }
 }
