927427c969280fd8bf2d8074c5d7431a3c298116
TimAndy <xuchonglei@126.com>
2017-10-23 18:03:52 +0800

    修改注释

--- payplatform-main/src/main/java/com/haiercash/payplatform/common/data/EntrySetting.java
+++ payplatform-main/src/main/java/com/haiercash/payplatform/common/data/EntrySetting.java
@@ -18,7 +18,7 @@ public class EntrySetting {
     private String activityUrl;//活动页地址   （域名省略，例如：/xjd/#!/applyQuota/applyQuota.html 以 / 开头）
     private String loginType;//01 登陆注册 02 三方免登陆
     private String verifyUrlService;//第三方验证的 service 的bean 目前支持: haierThirdTokenVerifyService
-    private String verifyUrlThird;//第三方token 验证地址 举例:  http://xxx/userinfo?access_token=
+    private String verifyUrlThird;//第三方token 验证地址 举例:  http://xxx/userinfo
     private String loginUrl;//登陆地址 举例: http://xx/login.html?token=
     private String loanTypeFrom;//贷款种类来源 01:channelNo 从后台取  02:根据 custName 从 CRM 取
     private String enabled;//Y 启用 N禁用
