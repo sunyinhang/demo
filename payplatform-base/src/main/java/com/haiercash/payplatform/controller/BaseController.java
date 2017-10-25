af8500901da1c749d600416e99a71157eb8d5720
TimAndy <xuchonglei@126.com>
2017-10-25 17:32:01 +0800

    精简优化代码

--- payplatform-base/src/main/java/com/haiercash/payplatform/controller/BaseController.java
+++ payplatform-base/src/main/java/com/haiercash/payplatform/controller/BaseController.java
@@ -1,14 +1,12 @@
 package com.haiercash.payplatform.controller;
 
 import com.bestvike.lang.StringUtils;
-import com.bestvike.lang.ThrowableUtils;
 import com.haiercash.commons.controller.AbstractController;
 import com.haiercash.payplatform.context.ThreadContext;
 import com.haiercash.payplatform.rest.common.CommonResponse;
+import com.haiercash.payplatform.servlet.ErrorHandler;
 import com.haiercash.payplatform.utils.BusinessException;
-import com.haiercash.payplatform.utils.ConstUtil;
 import com.haiercash.payplatform.utils.RestUtil;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,13 +19,7 @@ public class BaseController extends AbstractController {
     }
 
     protected Map<String, Object> fail(String retFlag, String retMsg) {
-        return RestUtil.fail(ConstUtil.APP_CODE + this.getModuleNo() + retFlag, retMsg);
-    }
-
-    protected Map<String, Object> fail(String retFlag, String retMsg, Object result) {
-        Map<String, Object> resultMap = RestUtil.fail(ConstUtil.APP_CODE + this.getModuleNo() + retFlag, retMsg);
-        resultMap.put("body", result);
-        return resultMap;
+        return RestUtil.fail(retFlag, retMsg);
     }
 
     protected Map<String, Object> success() {
@@ -60,15 +52,12 @@ public class BaseController extends AbstractController {
     @ResponseBody
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<CommonResponse> handleBusinessException(BusinessException e) {
-        CommonResponse response = CommonResponse.create(e.getRetFlag(), e.getRetMsg());
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return ErrorHandler.handleBusinessException(e);
     }
 
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public ResponseEntity<CommonResponse> handleException(Exception e) {
-        this.logger.error(ThrowableUtils.getString(e));
-        CommonResponse response = CommonResponse.create(ConstUtil.APP_CODE + this.getModuleNo() + ConstUtil.ERROR_CODE, ConstUtil.ERROR_INFO);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return ErrorHandler.handleException(e);
     }
 }
