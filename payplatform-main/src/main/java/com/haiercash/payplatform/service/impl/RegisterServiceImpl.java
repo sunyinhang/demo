6719fca9147a2f635f84488ba7a795459c425b14
lijianyu <lijianyu@bestvike.com>
2017-10-27 10:02:53 +0800

    修改绑定接口

--- payplatform-main/src/main/java/com/haiercash/payplatform/service/impl/RegisterServiceImpl.java
+++ payplatform-main/src/main/java/com/haiercash/payplatform/service/impl/RegisterServiceImpl.java
@@ -471,6 +471,11 @@ public class RegisterServiceImpl extends BaseService implements RegisterService 
     @Override
     public Map<String, Object> validateAndBindOtherUser(String token, String channel, String channelNo, Map<String, Object> params) throws Exception {
         Map<String, Object> map = new HashMap<>();
+        Map<String, Object> ifNeedFaceChkByTypCdeMap = new HashMap<String, Object>();
+        Map<String, Object> validateUserFlagMap = new HashMap<String, Object>();
+        Map<String, Object> hrparamMap = new HashMap<String, Object>();
+        String tag = "SHH";
+        String typCde = "";//贷款品种
         String password = (String) params.get("password");
         if (StringUtils.isEmpty(password)) {
             logger.info("password:" + password);
@@ -529,10 +534,7 @@ public class RegisterServiceImpl extends BaseService implements RegisterService 
         }
         if ("C1220".equals(custretflag)) {//C1120  客户信息不存在  跳转无额度页面
             session.set(token, cacheMap);
-//            String backurl = haiercashpay_web_url + "sgbt/#!/applyQuota/amountNot.html?token=" + token;
-//            map.put("backurl", backurl);
-//            logger.info("页面跳转到：" + backurl);
-            map.put("flag", "1");//  跳转无额度页面
+            map.put("flag", "6");//  跳转实名认证页面
             return success(map);
         }
         String certType = (String) ((Map<String, Object>) (custresult.get("body"))).get("certType");//证件类型
@@ -542,15 +544,6 @@ public class RegisterServiceImpl extends BaseService implements RegisterService 
         String cardNo = (String) ((Map<String, Object>) (custresult.get("body"))).get("cardNo");//银行卡号
         String bankNo = (String) ((Map<String, Object>) (custresult.get("body"))).get("acctBankNo");//银行代码
         String bankName = (String) ((Map<String, Object>) (custresult.get("body"))).get("acctBankName");//银行名称
-
-//        cacheMap.put("custNo", custNo);//客户编号
-//        cacheMap.put("name", custName);//客户姓名
-//        cacheMap.put("cardNo", cardNo);//银行卡号
-//        cacheMap.put("bankCode", bankNo);//银行代码
-//        cacheMap.put("bankName", bankName);//银行名称
-//        cacheMap.put("idNo", certNo);//身份证号
-//        cacheMap.put("idType", certType);
-
         cacheMap.put("custNo", custNo);//客户编号
         cacheMap.put("name", custName);//客户姓名
         cacheMap.put("cardNo", cardNo);//银行卡号
@@ -565,50 +558,164 @@ public class RegisterServiceImpl extends BaseService implements RegisterService 
         edMap.put("userId", uidLocal);//内部userId
         edMap.put("channel", "11");
         edMap.put("channelNo", channelNo);
-        Map edresult = appServerService.checkEdAppl(token, edMap);
-        if (!HttpUtil.isSuccess(edresult)) {//额度校验失败
-            String retmsg = (String) ((Map<String, Object>) (edresult.get("head"))).get("retMsg");
-            return fail(ConstUtil.ERROR_CODE, retmsg);
+        Map mapcache = appServerService.checkEdAppl(token, edMap);
+        logger.info("额度申请校验接口返回数据：" + mapcache);
+        if (!HttpUtil.isSuccess(mapcache)) {
+            Map<String, Object> head = (Map) mapcache.get("head");
+            String retFlag = (String) head.get("retFlag");
+            return fail(ConstUtil.ERROR_CODE, ConstUtil.ERROR_INFO);
         }
-        //获取自主支付可用额度金额
-        String crdNorAvailAmt = (String) ((Map<String, Object>) (edresult.get("body"))).get("crdNorAvailAmt");
-        if (crdNorAvailAmt != null && !"".equals(crdNorAvailAmt)) {
-            //跳转有额度页面
-//            String backurl = haiercashpay_web_url + "sgbt/#!/payByBt/myAmount.html?token=" + token;
-//            map.put("backurl", backurl);
-//            logger.info("页面跳转到：" + backurl);
-            map.put("flag", "2");// 跳转有额度页面
-            return success(map);
+        Object head2 = mapcache.get("head");
+        Map<String, Object> retinfo = (Map) head2;
+        String retFlag_ = (String) retinfo.get("retFlag");
+        String retMsg_ = (String) retinfo.get("retMsg");
+        if ("00000".equals(retFlag_)) {
+            Map<String, Object> headinfo = (Map) (mapcache.get("body"));
+            String applType = (String) headinfo.get("applType");
+            String flag = (String) headinfo.get("flag");
+            String outSts_f = (String) headinfo.get("outSts");
+            if ("25".equals(outSts_f)) {
+                map.put("flag", "10");// 拒绝
+                map.put("token", token);
+                return success(map);
+            }
+            String retmsg = "01";//未申请
+            if ("1".equals(applType) || ("".equals(applType) && "Y".equals(flag))) {
+//                if (hehyflag) {
+//                    resultparamMap.put("flag", "12");//预授信额度
+//                    resultparamMap.put("token", token);
+//                    return success(resultparamMap);
+//                }
+                logger.info("没有额度申请");
+                Map<String, Object> checkparamMap = new HashMap<String, Object>();
+                checkparamMap.put("channelNo", channelNo);
+                checkparamMap.put("tag", tag);//标签
+                checkparamMap.put("businessType", "EDJH");//业务类型 现金贷：XJD   商品分期：SPFQ      额度激活：EDJH    提额：TE   额度申请：EDSQ   个人信息维护：GRXX
+                checkparamMap.put("channel", channel);//渠道号
+                checkparamMap.put("isOrder", "N");//是否为订单
+                checkparamMap.put("orderNo", "");//订单编号
+                checkparamMap.put("applSeq", "");//订单流水号
+                checkparamMap.put("userId", uidLocal);//用户id
+                checkparamMap.put("custNo", custNo);//用户编号
+                checkparamMap.put("typCde", typCde);//贷款品种代码
+                checkparamMap.put("custName", custName);//用户名称
+                checkparamMap.put("noEduLocal", "NO");//是否校验最高学历与户口性质
+                checkparamMap.put("idNo", certNo);//身份证号
+                Map<String, Object> stringObjectMap = appServerService.checkIfMsgComplete(token, checkparamMap);
+                if (stringObjectMap == null) {
+                    return fail(ConstUtil.ERROR_CODE, ConstUtil.ERROR_INFO);
+                }
+                Map resultmapjsonMap = (Map<String, Object>) stringObjectMap.get("head");
+                String resultmapFlag = (String) resultmapjsonMap.get("retFlag");
+                if (!"00000".equals(resultmapFlag)) {
+                    String retMsg = (String) resultmapjsonMap.get("retMsg");
+                    return fail(ConstUtil.ERROR_CODE, retMsg);
+                }
+                Map resultmapbodyMap = (Map<String, Object>) stringObjectMap.get("body");
+                String SMRZ = (String) resultmapbodyMap.get("SMRZ");//实名认证信息
+                String GRJBXX = (String) resultmapbodyMap.get("GRJBXX");//个人基本信息
+                String DWXX = (String) resultmapbodyMap.get("DWXX");//单位信息
+                String JZXX = (String) resultmapbodyMap.get("JZXX");//居住信息
+                String LXRXX = (String) resultmapbodyMap.get("LXRXX");//联系人信息
+                Map BCYXMap = (Map<String, Object>) resultmapbodyMap.get("BCYX");//必传影像信息
+                String BCYX = (String) BCYXMap.get("BCYX");
+                if ("N".equals(SMRZ)) {
+                    //没有做过实名认证，跳转实名认证页面
+                    map.put("flag", "6");//转实名认证页面
+                } else {
+                    if ("Y".equals(GRJBXX) && "Y".equals(DWXX) && "Y".equals(JZXX) && "Y".equals(LXRXX) && "Y".equals(BCYX)) {//
+                        //如个人信息完整，则判断是否做过人脸识别
+                        ifNeedFaceChkByTypCdeMap.put("typCde", typCde);
+                        ifNeedFaceChkByTypCdeMap.put("source", channel);
+                        ifNeedFaceChkByTypCdeMap.put("custNo", custNo);
+                        ifNeedFaceChkByTypCdeMap.put("name", custName);
+                        ifNeedFaceChkByTypCdeMap.put("idNumber", certNo);
+                        ifNeedFaceChkByTypCdeMap.put("isEdAppl", "Y");
+                        Map<String, Object> saveCustFCiCustContactMap = appServerService.ifNeedFaceChkByTypCde(token, ifNeedFaceChkByTypCdeMap);
+                        if (saveCustFCiCustContactMap == null) {
+                            return fail(ConstUtil.ERROR_CODE, ConstUtil.ERROR_INFO);
+                        }
+                        Map saveCustFCiCustContactMapHeadMap = (Map<String, Object>) saveCustFCiCustContactMap.get("head");
+                        String saveCustFCiCustContactMapHeadFlag = (String) saveCustFCiCustContactMapHeadMap.get("retFlag");
+                        if (!"00000".equals(saveCustFCiCustContactMapHeadFlag)) {
+                            String retMsg = (String) saveCustFCiCustContactMapHeadMap.get("retMsg");
+                            return fail(ConstUtil.ERROR_CODE, retMsg);
+                        }
+                        Map saveCustFCiCustContactMapBodyMap = (Map<String, Object>) saveCustFCiCustContactMap.get("body");
+                        String code = (String) saveCustFCiCustContactMapBodyMap.get("code");
+                        if (code != null && !"".equals(code)) {
+                            logger.info("*********人脸识别标识码：" + code);
+                            if ("00".equals(code)) {// 00：已经通过了人脸识别（得分合格），不需要再做人脸识别
+                                validateUserFlagMap.put("channelNo", channelNo);// 渠道
+                                validateUserFlagMap.put("channel", channel);
+                                validateUserFlagMap.put("userId", EncryptUtil.simpleEncrypt(uidLocal));//客户编号18254561920
+                                Map<String, Object> alidateUserMap = appServerService.validateUserFlag(token, validateUserFlagMap);
+                                if (alidateUserMap == null) {
+                                    return fail(ConstUtil.ERROR_CODE, ConstUtil.ERROR_INFO);
+                                }
+                                Map alidateUserHeadMap = (Map<String, Object>) alidateUserMap.get("head");
+                                String alidateUserHeadMapFlag = (String) alidateUserHeadMap.get("retFlag");
+                                if (!"00000".equals(alidateUserHeadMapFlag)) {
+                                    String retMsg = (String) alidateUserHeadMap.get("retMsg");
+                                    return fail(ConstUtil.ERROR_CODE, retMsg);
+                                }
+                                Map alidateUserBodyMap = (Map<String, Object>) alidateUserMap.get("body");
+                                String payPasswdFlag = (String) alidateUserBodyMap.get("payPasswdFlag");
+                                if (payPasswdFlag == null || "".equals(payPasswdFlag)) {
+                                    return fail(ConstUtil.ERROR_CODE, ConstUtil.ERROR_INFO);
+                                }
+                                if ("1".equals(payPasswdFlag)) {//1.已设置支付密码
+                                    map.put("flag", "1");
+                                } else {//没有设置支付密码
+                                    map.put("flag", "2");
+                                }
+                            } else if ("01".equals(code)) {// 01：未通过人脸识别，剩余次数为0，不能再做人脸识别，录单终止
+                                map.put("flag", "3");
+                            } else if ("02".equals(code)) {// 02：未通过人脸识别，剩余次数为0，不能再做人脸识别，但可以上传替代影像
+                                map.put("flag", "4");
+                            } else {//跳转人脸识别
+                                map.put("flag", "5");
+                            }
+
+                        }
+                    } else {
+                        //个人信息不完成，跳转完善个人扩展信息页面
+                        map.put("flag", "7");//跳转完善个人扩展信息页面
+                    }
+                }
+            } else if ("2".equals(applType)) {
+                HashMap<String, Object> edCheckmap = new HashMap<>();
+                edCheckmap.put("idNo", certNo);
+                edCheckmap.put("channel", "11");
+                edCheckmap.put("channelNo", channelNo);
+                edCheckmap.put("idTyp", certType);
+                Map<String, Object> edApplProgress = appServerService.getEdApplProgress(null, edCheckmap);//(POST)额度申请进度查询（最新的进度 根据idNo查询）
+                Map<String, Object> head = (Map) edApplProgress.get("head");
+                if (!"00000".equals(head.get("retFlag"))) {
+                    logger.info("额度申请进度查询（最新的进度 根据idNo查询）,错误信息：" + head.get("retMsg"));
+                    return fail(ConstUtil.ERROR_CODE, (String) head.get("retMsg"));
+                }
+                Map<String, Object> body = (Map) edApplProgress.get("body");
+                Integer crdSeqInt = (Integer) body.get("applSeq");
+                String crdSeq = Integer.toString(crdSeqInt);
+                cacheMap.put("crdSeq", crdSeq);
+                session.set(token, cacheMap);
+                String outSts = body.get("outSts").toString();
+                if ("27".equals(outSts)) {
+                    map.put("flag", "9");//通过  我的额度
+                } else if ("25".equals(outSts)) {
+                    map.put("flag", "10");// 拒绝
+                } else if ("22".equals(outSts)) {
+                    map.put("flag", "11");// 退回
+                } else {//审批中
+                    map.put("flag", "8");// 审批中
+                }
+            } else if ("".equals(flag)) {
+                map.put("flag", "9");//通过  我的额度
+            }
+
         }
-        //审批状态判断
-        String outSts = (String) ((Map<String, Object>) (edresult.get("body"))).get("outSts");
-        if ("01".equals(outSts)) {//额度正在审批中
-//            String backurl = haiercashpay_web_url + "sgbt/#!/applyQuota/applyIn.html?token=" + token;
-//            map.put("backurl", backurl);
-//            logger.info("页面跳转到：" + backurl);
-            map.put("flag", "3");//额度正在审批中
-            return success(map);
-        } else if ("22".equals(outSts)) {//审批被退回
-            String crdSeq = (String) ((Map<String, Object>) (edresult.get("body"))).get("crdSeq");
-            cacheMap.put("crdSeq", crdSeq);
-            session.set(token, cacheMap);
-//            String backurl = haiercashpay_web_url + "sgbt/#!/applyQuota/applyReturn.html?token=" + token;
-//            map.put("backurl", backurl);
-//            logger.info("页面跳转到：" + backurl);
-            map.put("flag", "4");//审批被退回
-            return success(map);
-        } else if ("25".equals(outSts)) {//审批被拒绝
-//            String backurl = haiercashpay_web_url + "sgbt/#!/applyQuota/applyFail.html?token=" + token;
-//            map.put("backurl", backurl);
-//            logger.info("页面跳转到：" + backurl);
-            map.put("flag", "5");//审批被拒绝
-            return success(map);
-        } else {//没有额度  额度激活
-//            String backurl = haiercashpay_web_url + "sgbt/#!/applyQuota/amountActive.html?token=" + token;
-//            map.put("backurl", backurl);
-//            logger.info("页面跳转到：" + backurl);
-            map.put("flag", "6");//没有额度  额度激活
-            return success(map);
+        map.put("token", token);
+        return success(map);
         }
     }
-}
