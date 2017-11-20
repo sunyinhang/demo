51eee4c8d63062df5bdf84f4b6d0215c5b49dbc9
TimAndy <xuchonglei@126.com>
2017-11-20 11:27:54 +0800

    ClientUtils 重构

--- haiercash-spring/src/main/java/com/haiercash/spring/rest/AbstractClientUtils.java
+++ haiercash-spring/src/main/java/com/haiercash/spring/rest/AbstractClientUtils.java
@@ -2,6 +2,7 @@ package com.haiercash.spring.rest;
 
 import com.haiercash.core.collection.MapUtils;
 import com.haiercash.core.lang.Convert;
+import com.haiercash.core.reflect.GenericTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -10,27 +11,33 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.util.Map;
 
 /**
  * Created by 许崇雷 on 2017-10-21.
  */
-public abstract class AbstractClientUtils {
+public abstract class AbstractClientUtils<TResponse> {
     private static final String ERROR_NULL_MSG = "外部服务未返回任何数据";
     private static final String ERROR_SERVER_MSG = "外部系统发生错误:HTTP-%s";
 
     protected abstract RestTemplate getRestTemplate();
 
-    public String getForStringCore(String url, Map<String, ?> uriVariables, MultiValueMap<String, String> headers) {
+    protected final GenericTypeReference<TResponse> createResponseTypeReference(Type bodyType) {
+        return new GenericTypeReference<>(bodyType);
+    }
+
+    public TResponse getForCore(String url, Type bodyType, Map<String, ?> uriVariables, MultiValueMap<String, String> headers) {
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url);
         if (MapUtils.isNotEmpty(uriVariables)) {
             for (Map.Entry<String, ?> entry : uriVariables.entrySet())
                 uriBuilder.queryParam(entry.getKey(), Convert.toStringHuman(entry.getValue()));
         }
         URI uri = uriBuilder.build().encode().toUri();
+        GenericTypeReference<TResponse> responseTypeReference = this.createResponseTypeReference(bodyType);
         RestTemplate restTemplate = this.getRestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        ResponseEntity<TResponse> responseEntity = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<>(headers), responseTypeReference);
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
             if (responseEntity.getBody() == null)
                 throw new RuntimeException(ERROR_NULL_MSG);
@@ -39,15 +46,16 @@ public abstract class AbstractClientUtils {
         throw new RuntimeException(String.format(ERROR_SERVER_MSG, responseEntity.getStatusCodeValue()));
     }
 
-    public String deleteForStringCore(String url, Map<String, ?> uriVariables, MultiValueMap<String, String> headers) {
+    public TResponse deleteForCore(String url, Type bodyType, Map<String, ?> uriVariables, MultiValueMap<String, String> headers) {
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url);
         if (MapUtils.isNotEmpty(uriVariables)) {
             for (Map.Entry<String, ?> entry : uriVariables.entrySet())
                 uriBuilder.queryParam(entry.getKey(), Convert.toStringHuman(entry.getValue()));
         }
         URI uri = uriBuilder.build().encode().toUri();
+        GenericTypeReference<TResponse> responseTypeReference = this.createResponseTypeReference(bodyType);
         RestTemplate restTemplate = this.getRestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.DELETE, new HttpEntity<>(headers), String.class);
+        ResponseEntity<TResponse> responseEntity = restTemplate.exchange(uri, HttpMethod.DELETE, new HttpEntity<>(headers), responseTypeReference);
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
             if (responseEntity.getBody() == null)
                 throw new RuntimeException(ERROR_NULL_MSG);
@@ -56,11 +64,12 @@ public abstract class AbstractClientUtils {
         throw new RuntimeException(String.format(ERROR_SERVER_MSG, responseEntity.getStatusCodeValue()));
     }
 
-    public String postForStringCore(String url, Object request, MultiValueMap<String, String> headers) {
+    public TResponse postForCore(String url, Object request, Type bodyType, MultiValueMap<String, String> headers) {
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url);
         URI uri = uriBuilder.build().encode().toUri();
+        GenericTypeReference<TResponse> responseTypeReference = this.createResponseTypeReference(bodyType);
         RestTemplate restTemplate = this.getRestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(request, headers), String.class);
+        ResponseEntity<TResponse> responseEntity = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(request, headers), responseTypeReference);
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
             if (responseEntity.getBody() == null)
                 throw new RuntimeException(ERROR_NULL_MSG);
@@ -69,11 +78,12 @@ public abstract class AbstractClientUtils {
         throw new RuntimeException(String.format(ERROR_SERVER_MSG, responseEntity.getStatusCodeValue()));
     }
 
-    public String putForStringCore(String url, Object request, MultiValueMap<String, String> headers) {
+    public TResponse putForCore(String url, Object request, Type bodyType, MultiValueMap<String, String> headers) {
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url);
         URI uri = uriBuilder.build().encode().toUri();
+        GenericTypeReference<TResponse> responseTypeReference = this.createResponseTypeReference(bodyType);
         RestTemplate restTemplate = this.getRestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.exchange(uri, HttpMethod.PUT, new HttpEntity<>(request, headers), String.class);
+        ResponseEntity<TResponse> responseEntity = restTemplate.exchange(uri, HttpMethod.PUT, new HttpEntity<>(request, headers), responseTypeReference);
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
             if (responseEntity.getBody() == null)
                 throw new RuntimeException(ERROR_NULL_MSG);
