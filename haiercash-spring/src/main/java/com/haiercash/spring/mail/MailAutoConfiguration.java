a46d79e9e4cc8835c1f0c8f7ba0a3c591a58729a
TimAndy <xuchonglei@126.com>
2017-11-23 11:00:43 +0800

    不配置 smtp 时允许程序启动,但发送邮件时警告未配置 smtp

--- haiercash-spring/src/main/java/com/haiercash/spring/mail/MailAutoConfiguration.java
+++ haiercash-spring/src/main/java/com/haiercash/spring/mail/MailAutoConfiguration.java
@@ -1,8 +1,10 @@
 package com.haiercash.spring.mail;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.AnyNestedCondition;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 
@@ -10,7 +12,7 @@ import org.springframework.mail.javamail.JavaMailSender;
  * Created by 许崇雷 on 2017-11-21.
  */
 @Configuration
-@ConditionalOnBean(JavaMailSender.class)
+@Conditional(MailAutoConfiguration.MailSenderCondition.class)
 @EnableConfigurationProperties(MailProperties.class)
 public class MailAutoConfiguration {
     private final JavaMailSender mailSender;
@@ -24,5 +26,24 @@ public class MailAutoConfiguration {
     @Bean
     MailTemplate mailTemplate() {
         return new MailTemplate(this.mailSender, this.properties);
+    }
+
+
+    static class MailSenderCondition extends AnyNestedCondition {
+        MailSenderCondition() {
+            super(ConfigurationPhase.PARSE_CONFIGURATION);
+        }
+
+        @ConditionalOnProperty(prefix = "spring.mail", name = "jndi-name")
+        static class JndiNameProperty {
+            JndiNameProperty() {
+            }
+        }
+
+        @ConditionalOnProperty(prefix = "spring.mail", name = "host")
+        static class HostProperty {
+            HostProperty() {
+            }
+        }
     }
 }
