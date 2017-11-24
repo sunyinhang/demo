280c211349a7a03c3f2f7acfbf2a266d4a0c794b
TimAndy <xuchonglei@126.com>
2017-11-24 16:21:31 +0800

    RabbitInfo 支持 clone()

--- haiercash-spring/src/main/java/com/haiercash/spring/rabbit/RabbitInfo.java
+++ haiercash-spring/src/main/java/com/haiercash/spring/rabbit/RabbitInfo.java
@@ -1,12 +1,15 @@
 package com.haiercash.spring.rabbit;
 
+import com.haiercash.core.lang.Cloneable;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * Created by 许崇雷 on 2017-11-01.
  */
 @Data
-public final class RabbitInfo {
+@EqualsAndHashCode(callSuper = false)
+public final class RabbitInfo extends Cloneable<RabbitInfo> {
     private String exchange;
     private String routingKey;
     private String queue;
