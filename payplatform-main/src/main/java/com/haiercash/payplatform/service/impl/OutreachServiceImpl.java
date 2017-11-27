6203eaa9da2df494ea0b7ab83bef087d818ad107
yujianwei <yujianwei@bestvike.com>
2017-11-27 16:08:14 +0800

    芝麻授权 接口调整

--- payplatform-main/src/main/java/com/haiercash/payplatform/service/impl/OutreachServiceImpl.java
+++ payplatform-main/src/main/java/com/haiercash/payplatform/service/impl/OutreachServiceImpl.java
@@ -2,7 +2,6 @@ package com.haiercash.payplatform.service.impl;
 
 import com.haiercash.payplatform.service.OutreachService;
 import com.haiercash.spring.rest.client.JsonClientUtils;
-import com.haiercash.spring.utils.HttpUtil;
 
 import java.util.Map;
 
@@ -14,9 +13,7 @@ public class OutreachServiceImpl extends FaceServiceImpl implements OutreachServ
     @Override
     public Map<String, Object> protocolauth(Map<String, Object> params) {
         String url = outplatform_url+"/Outreachplatform/api/alibaba/protocolauth";
-        String resData = JsonClientUtils.postForString(url, params);
-        logger.info("调用外联人脸识别接口，返回数据：" + resData);
-        return HttpUtil.json2Map(resData);
+        return  JsonClientUtils.postForMap(url, params);
     }
 
 
