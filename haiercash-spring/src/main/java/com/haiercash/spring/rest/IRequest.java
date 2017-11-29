ab8403fa641b701ad6b8088584599b710def4577
TimAndy <xuchonglei@126.com>
2017-11-29 16:34:39 +0800

    添加 CmisAcqRequest 和 Builder

---
+++ haiercash-spring/src/main/java/com/haiercash/spring/rest/IRequest.java
@@ -0,0 +1,7 @@
+package com.haiercash.spring.rest;
+
+/**
+ * Created by 许崇雷 on 2017-11-29.
+ */
+public interface IRequest {
+}
---
+++ haiercash-spring/src/main/java/com/haiercash/spring/rest/cmisacq/CmisAcqRequestHead.java
@@ -0,0 +1,34 @@
+package com.haiercash.spring.rest.cmisacq;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
+
+/**
+ * Created by 许崇雷 on 2017-11-29.
+ */
+@Data
+public final class CmisAcqRequestHead {
+    @JSONField(ordinal = 1)
+    private String tradeCode;
+
+    @JSONField(ordinal = 2)
+    private String serno;
+
+    @JSONField(ordinal = 3)
+    private String sysFlag;
+
+    @JSONField(ordinal = 4)
+    private String tradeType;
+
+    @JSONField(ordinal = 5)
+    private String tradeDate;
+
+    @JSONField(ordinal = 6)
+    private String tradeTime;
+
+    @JSONField(ordinal = 7)
+    private String channelNo;
+
+    @JSONField(ordinal = 8)
+    private String cooprCode;
+}
