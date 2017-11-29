7d453d151eab6e0e4956ca2bd55e1e0805d1ad9e
TimAndy <xuchonglei@126.com>
2017-11-29 16:46:11 +0800

    修复单元测试

--- haiercash-spring/src/test/java/com/haiercash/spring/mail/bugreport/BugReportLevelTest.java
+++ haiercash-spring/src/test/java/com/haiercash/spring/mail/bugreport/BugReportLevelTest.java
@@ -10,7 +10,10 @@ public class BugReportLevelTest {
     @Test
     public void testBugReportLevel() {
         Assert.assertEquals("INFO", BugReportLevel.INFO.toString());
-        Assert.assertEquals("INFO", BugReportLevel.INFO.getName());
+        Assert.assertEquals("INFO", BugReportLevel.INFO.name());
+        Assert.assertEquals(0, BugReportLevel.INFO.ordinal());
+        Assert.assertEquals(1, BugReportLevel.WARN.ordinal());
+        Assert.assertEquals(2, BugReportLevel.ERROR.ordinal());
         Assert.assertEquals("消息", BugReportLevel.INFO.getDescription());
     }
 }
