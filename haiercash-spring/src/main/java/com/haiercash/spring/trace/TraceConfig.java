83a626e740fec8752cf53d2e1f40165f62a8b8c8
TimAndy <xuchonglei@126.com>
2017-11-30 08:47:11 +0800

    使用 StandardCharsets 直接使用 Charset 实例

--- haiercash-spring/src/main/java/com/haiercash/spring/trace/TraceConfig.java
+++ haiercash-spring/src/main/java/com/haiercash/spring/trace/TraceConfig.java
@@ -1,8 +1,7 @@
 package com.haiercash.spring.trace;
 
-import com.haiercash.core.io.CharsetNames;
-
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by 许崇雷 on 2017-10-21.
@@ -13,6 +12,6 @@ public final class TraceConfig {
     public static final String BODY_PARSE_FAIL = "内容转换为字符串失败";
     public static final String BODY_OVER_FLOW = "(...内容过大，无法显示)";
     public static final String BODY_RESOURCE = "(...资源文件，无法显示)";
-    public static final Charset DEFAULT_CHARSET = Charset.forName(CharsetNames.UTF_8);
+    public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
     public static final ThreadLocal<byte[]> BUFFER = ThreadLocal.withInitial(() -> new byte[BUFFER_SIZE]);
 }
