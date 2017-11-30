87480c0769166dbdeed3d8b2dd75ca9388192b86
TimAndy <xuchonglei@126.com>
2017-11-30 08:27:04 +0800

    添加单元测试

---
+++ haiercash-core/src/test/java/com/haiercash/core/lang/ConvertTest.java
@@ -0,0 +1,21 @@
+package com.haiercash.core.lang;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * Created by 许崇雷 on 2017-11-30.
+ */
+public class ConvertTest {
+    @Test
+    public void toType() throws Exception {
+        String str = "2017-1-2 01:02:03";
+        Date date = Convert.toType(str, Date.class);
+        Assert.assertEquals(Date.class, date.getClass());
+        Timestamp timestamp = Convert.toType(str, Timestamp.class);
+        Assert.assertEquals(Timestamp.class, timestamp.getClass());
+    }
+}
