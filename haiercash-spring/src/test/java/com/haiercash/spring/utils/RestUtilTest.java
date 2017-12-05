a4a6296f9221107fe5275ff45539fe405d61c9f6
TimAndy <xuchonglei@126.com>
2017-12-05 18:10:12 +0800

    添加单元测试

---
+++ haiercash-spring/src/test/java/com/haiercash/spring/utils/RestUtilTest.java
@@ -0,0 +1,23 @@
+package com.haiercash.spring.utils;
+
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by 许崇雷 on 2017-12-05.
+ */
+public class RestUtilTest {
+    @Test
+    public void success() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("userId", "222");
+        Map map = RestUtil.success(body);
+        JSONObject jsonObject = new JSONObject((Map) map.get("head"));//此处必须拆箱为 Map
+        String retFlag = (String) jsonObject.get("retFlag");
+        Assert.assertEquals("00000", retFlag);
+    }
+}
