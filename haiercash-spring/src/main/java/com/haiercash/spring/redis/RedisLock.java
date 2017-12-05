14f7b7cf45f406bf925bb42abd5c58f4ce41994c
TimAndy <xuchonglei@126.com>
2017-12-05 14:51:15 +0800

    fix 不等待时,可能导致永远无法获取到锁的问题

--- haiercash-spring/src/main/java/com/haiercash/spring/redis/RedisLock.java
+++ haiercash-spring/src/main/java/com/haiercash/spring/redis/RedisLock.java
@@ -17,11 +17,11 @@ public final class RedisLock {
     private static final int DEFAULT_SLEEP_MILLIS = 100;//加锁等待时间片
     private final String name;//名称
     private final String key;//lock key
-    private final int waitTime;//申请过程的等待时间,非正数不等待. 毫秒
+    private final int waitMillis;//申请过程的等待毫秒数,大于零时才等待
     private boolean locked;//是否加锁
 
     /**
-     * 构造函数, 默认申请过程的超时时间为 5000 毫秒
+     * 构造函数, 默认不等待
      *
      * @param name 锁名称, 会自动添加全局前缀
      */
@@ -32,14 +32,14 @@ public final class RedisLock {
     /**
      * 构造函数
      *
-     * @param name     锁名称, 会自动添加全局前缀
-     * @param waitTime 申请过程的等待时间,非正数不等待. 毫秒
+     * @param name       锁名称, 会自动添加全局前缀
+     * @param waitMillis 申请过程的等待毫秒数,大于零时才等待
      */
-    public RedisLock(String name, int waitTime) {
+    public RedisLock(String name, int waitMillis) {
         Assert.hasLength(name, "name can not be empty");
         this.name = name;
         this.key = name + "_LOCK";
-        this.waitTime = waitTime;
+        this.waitMillis = waitMillis;
     }
 
     /**
@@ -48,13 +48,13 @@ public final class RedisLock {
      * @return 锁名称
      */
     public String getName() {
-        return name;
+        return this.name;
     }
 
     /**
      * 加锁
      *
-     * @param leaseTime 最大持有时间
+     * @param leaseTime 租借时间
      * @param unit      时间单位
      * @return 加锁成功返回 true, 否则返回 false
      */
@@ -63,36 +63,24 @@ public final class RedisLock {
             throw new ArgumentOutOfRangeException("leaseTime", "leaseTime must greater than zero");
         if (this.locked)
             throw new InvalidOperationException("unable to lock repeatedly");
-        //非等待
-        long expireTimeout = TimeoutUtils.toMillis(leaseTime, unit);
-        if (this.waitTime <= 0)
-            return this.locked = RedisUtils.setnx(this.key, System.currentTimeMillis() + expireTimeout);
-        //等待
-        long waitEnd = System.currentTimeMillis() + this.waitTime;
+        //加锁
+        boolean shouldWait = this.waitMillis > 0;//应该等待
+        long waitEnd = System.currentTimeMillis() + this.waitMillis;//等待截止时刻
+        long leaseMillis = TimeoutUtils.toMillis(leaseTime, unit);//租借毫秒数
         do {
-            //尝试加锁,值为到期时间
-            if (RedisUtils.setnx(this.key, System.currentTimeMillis() + expireTimeout))
-                return this.locked = true;
-
-            //别人已释放,立即进入下次循环,尝试加锁
-            Long expireEndInRedis = RedisUtils.get(this.key, Long.class); //redis里的时间
-            if (expireEndInRedis == null)
-                continue;
-
-            //未过期等待
-            if (System.currentTimeMillis() <= expireEndInRedis) {
-                ThreadUtils.sleep(DEFAULT_SLEEP_MILLIS);
-                continue;
+            switch (this.tryLock(leaseMillis)) {
+                case SUCCESS:
+                    return this.locked = true;
+                case RETRY:
+                    break;
+                default:
+                    if (shouldWait && System.currentTimeMillis() <= waitEnd) {
+                        ThreadUtils.sleep(DEFAULT_SLEEP_MILLIS);
+                        break;
+                    }
+                    return this.locked = false;
             }
-
-            //过期覆盖. 可能覆盖两次, 但是因为什么相差了很少的时间，所以可以接受.如果获取到的为 null 说明之前被删,正好加锁成功
-            Long expireEndInRedisNow = RedisUtils.getset(this.key, System.currentTimeMillis() + expireTimeout, Long.class);
-            if (expireEndInRedisNow == null || Objects.equals(expireEndInRedis, expireEndInRedisNow))
-                return this.locked = true;
-            ThreadUtils.sleep(DEFAULT_SLEEP_MILLIS);
-        } while (System.currentTimeMillis() <= waitEnd);
-        //超过等待时间
-        return this.locked = false;
+        } while (true);
     }
 
     /**
@@ -103,5 +91,39 @@ public final class RedisLock {
             RedisUtils.del(this.key);
             this.locked = false;
         }
+    }
+
+    /**
+     * 尝试加锁
+     *
+     * @param leaseMillis 租借毫秒数
+     * @return 加锁结果
+     */
+    private LockResult tryLock(long leaseMillis) {
+        //尝试加锁,值为到期时间
+        if (RedisUtils.setnx(this.key, System.currentTimeMillis() + leaseMillis))
+            return LockResult.SUCCESS;
+        //别人已释放,立即进入下次循环,尝试加锁
+        Long expireEnd = RedisUtils.get(this.key, Long.class); //redis里的时间
+        if (expireEnd == null)
+            return LockResult.RETRY;
+        //未过期等待
+        if (System.currentTimeMillis() <= expireEnd)
+            return LockResult.FAIL;
+        //过期覆盖. 可能覆盖两次, 但是因为什么相差了很少的时间，所以可以接受.如果获取到的为 null 说明之前被删,正好加锁成功
+        Long expireEndNow = RedisUtils.getset(this.key, System.currentTimeMillis() + leaseMillis, Long.class);
+        if (expireEndNow == null || Objects.equals(expireEnd, expireEndNow))
+            return LockResult.SUCCESS;
+        return LockResult.FAIL;
+    }
+
+    //加锁结果
+    private enum LockResult {
+        //加锁失败,需要等待然后执行下次加锁
+        FAIL,
+        //加锁失败,无需等待立即执行下次加锁
+        RETRY,
+        //加锁成功
+        SUCCESS
     }
 }
