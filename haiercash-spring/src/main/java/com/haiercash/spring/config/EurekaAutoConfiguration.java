7502579008ec0a566b144777afb6f8f185bd62c0
TimAndy <xuchonglei@126.com>
2018-01-09 15:28:31 +0800

    配置调整,添加 CMIS-INTERFACE-SERVER

---
+++ haiercash-spring/src/main/java/com/haiercash/spring/config/EurekaAutoConfiguration.java
@@ -0,0 +1,30 @@
+package com.haiercash.spring.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by 许崇雷 on 2018-01-09.
+ */
+@Configuration
+@EnableConfigurationProperties(EurekaProperties.class)
+public class EurekaAutoConfiguration {
+    public EurekaAutoConfiguration(EurekaProperties eurekaProperties) {
+        EurekaServer.ACQUIRER = eurekaProperties.getACQUIRER();
+        EurekaServer.APPCA = eurekaProperties.getAPPCA();
+        EurekaServer.APPMANAGE = eurekaProperties.getAPPMANAGE();
+        EurekaServer.APPMSG = eurekaProperties.getAPPMSG();
+        EurekaServer.APPSERVER = eurekaProperties.getAPPSERVER();
+        EurekaServer.APPSERVERNOAUTH = eurekaProperties.getAPPSERVERNOAUTH();
+        EurekaServer.APPSERVERNOAUTHNEW = eurekaProperties.getAPPSERVERNOAUTHNEW();
+        EurekaServer.CMISFRONTSERVER = eurekaProperties.getCMISFRONTSERVER();
+        EurekaServer.CMISINTERFACESERVER = eurekaProperties.getCMISINTERFACESERVER();
+        EurekaServer.CMISPROXY = eurekaProperties.getCMISPROXY();
+        EurekaServer.CRM = eurekaProperties.getCRM();
+        EurekaServer.GM = eurekaProperties.getGM();
+        EurekaServer.HCPORTAL = eurekaProperties.getHCPORTAL();
+        EurekaServer.ORDER = eurekaProperties.getORDER();
+        EurekaServer.OUTREACHPLATFORM = eurekaProperties.getOUTREACHPLATFORM();
+        EurekaServer.UAUTH = eurekaProperties.getUAUTH();
+    }
+}
