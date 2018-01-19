f5cc0ad1756d5d6a3b94d88bf0c741c7f5104fcd
TimAndy <xuchonglei@126.com>
2018-01-19 17:36:37 +0800

    生活号联合登陆

--- payplatform-main/src/main/java/com/haiercash/payplatform/pc/alipay/fuwu/controller/AlipayFuwuController.java
+++ payplatform-main/src/main/java/com/haiercash/payplatform/pc/alipay/fuwu/controller/AlipayFuwuController.java
@@ -1,19 +1,76 @@
 package com.haiercash.payplatform.pc.alipay.fuwu.controller;
 
+import com.alipay.api.AlipayApiException;
+import com.haiercash.core.lang.StringUtils;
+import com.haiercash.payplatform.config.AlipayConfig;
 import com.haiercash.payplatform.pc.alipay.fuwu.service.AlipayFuwuService;
 import com.haiercash.spring.controller.BaseController;
+import com.haiercash.spring.rest.IResponse;
+import com.haiercash.spring.rest.common.CommonResponse;
+import com.haiercash.spring.util.ConstUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by 许崇雷 on 2018-01-18.
  */
 @RestController
 public class AlipayFuwuController extends BaseController {
+    private static final String ERROR_URL = "/error";
+    private static final String CHANNEL_NO = "127";
+    @Autowired
+    private AlipayConfig alipayConfig;
+
+
     @Autowired
     private AlipayFuwuService alipayFuwuService;
 
     public AlipayFuwuController() {
         super("17");
+    }
+
+    //配置获取
+    @GetMapping("/api/payment/alipay/fuwu/config")
+    public IResponse<Map> getConfig() {
+        Map<String, Object> body = new HashMap<>();
+        body.put("appId", alipayConfig.getAppId());
+        body.put("channelNo", CHANNEL_NO);
+        return CommonResponse.success(body);
+    }
+
+    //联合登陆
+    @GetMapping("/api/payment/alipay/fuwu/login")
+    public IResponse<Map> login(@RequestParam Map<String, String> params) throws AlipayApiException {
+        String appId = params.get("appId");
+        if (!Objects.equals(appId, alipayConfig.getAppId()))
+            return CommonResponse.fail(ConstUtil.ERROR_CODE, "错误的 appId");
+        String authCode = params.get("auth_code");
+        if (StringUtils.isEmpty(authCode))
+            return CommonResponse.fail(ConstUtil.ERROR_CODE, "auth_code 不能为空");
+        String channelNo = this.getChannelNo();
+        if (StringUtils.isEmpty(channelNo))
+            return CommonResponse.fail(ConstUtil.ERROR_CODE, "渠道号不能为空");
+        return alipayFuwuService.login(authCode);
+    }
+
+
+    //额度入口
+    @GetMapping("/api/payment/alipay/fuwu/creditEntry")
+    public IResponse<Map> creditEntry(@RequestParam Map<String, String> params) throws IOException, AlipayApiException {
+        String appId = params.get("appId");
+        if (!Objects.equals(appId, alipayConfig.getAppId()))
+            return CommonResponse.fail(ConstUtil.ERROR_CODE, "错误的 appId");
+        String authCode = params.get("auth_code");
+        if (StringUtils.isEmpty(authCode))
+            return CommonResponse.fail(ConstUtil.ERROR_CODE, "auth_code 不能为空");
+
+        return alipayFuwuService.creditEntry(authCode);
     }
 }
--- payplatform-main/src/main/java/com/haiercash/payplatform/pc/alipay/fuwu/service/AlipayFuwuService.java
+++ payplatform-main/src/main/java/com/haiercash/payplatform/pc/alipay/fuwu/service/AlipayFuwuService.java
@@ -1,11 +1,149 @@
 package com.haiercash.payplatform.pc.alipay.fuwu.service;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.response.AlipayUserInfoShareResponse;
+import com.haiercash.payplatform.pc.alipay.bean.AlipayToken;
+import com.haiercash.payplatform.pc.alipay.util.AlipayUtils;
+import com.haiercash.payplatform.utils.AppServerUtils;
+import com.haiercash.payplatform.utils.EncryptUtil;
+import com.haiercash.spring.config.EurekaServer;
+import com.haiercash.spring.redis.RedisUtils;
+import com.haiercash.spring.rest.IResponse;
+import com.haiercash.spring.rest.common.CommonResponse;
+import com.haiercash.spring.rest.common.CommonRestUtils;
 import com.haiercash.spring.service.BaseService;
+import com.haiercash.spring.util.ConstUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by 许崇雷 on 2018-01-18.
  */
 @Service
 public class AlipayFuwuService extends BaseService {
+    //联合登陆 auth_base 模式
+    public IResponse<Map> login(String authCode) throws AlipayApiException {
+        String channelNo = getChannelNo();
+        AlipayToken token = AlipayUtils.getOauthTokenByAuthCode(authCode);
+        String thirdUserId = token.getUserId();
+        String makeToken = channelNo + "-" + thirdUserId;//自定义 token:渠道号+第三方 userId
+
+        //查询第三方账号信息
+        IResponse<Map> externUidResp = this.queryUserByExternUid(thirdUserId);
+        if (!externUidResp.isSuccessNeedBody()) {
+            Map<String, Object> body = new HashMap<>();
+            body.put("flag", "01");//失败 -> loading 页
+            body.put("token", makeToken);
+            return CommonResponse.success(body);
+        }
+        Map<String, String> externUidBody = externUidResp.getBody();
+        String userId = externUidBody.get("userId");
+        String phone = externUidBody.get("mobile");
+
+        //查询个人资料
+        IResponse<Map> custInfoResp = this.queryPerCustInfo(userId);
+        if (!custInfoResp.isSuccessNeedBody()) {
+            Map<String, Object> body = new HashMap<>();
+            body.put("flag", "02");//资料录入
+            body.put("token", makeToken);
+            return CommonResponse.success(body);
+        }
+        Map<String, String> custInfoBody = custInfoResp.getBody();
+
+        //将个人资料存入 redis
+        Map<String, Object> cachemap = new HashMap<>();
+        cachemap.put("userId", userId);
+        cachemap.put("phoneNo", phone);
+        cachemap.put("custNo", custInfoBody.get("custNo"));//客户编号
+        cachemap.put("name", custInfoBody.get("custName"));//客户姓名
+        cachemap.put("cardNo", custInfoBody.get("cardNo"));//银行卡号
+        cachemap.put("bankCode", custInfoBody.get("acctBankNo"));//银行代码
+        cachemap.put("bankName", custInfoBody.get("acctBankName"));//银行名称
+        cachemap.put("idNo", custInfoBody.get("certNo"));//身份证号
+        cachemap.put("idCard", custInfoBody.get("certNo"));//身份证号
+        cachemap.put("idType", custInfoBody.get("certType"));
+        RedisUtils.setExpire(makeToken, cachemap);
+
+        //查询额度信息
+        IResponse<Map> checkEdResp = this.checkEdAppl(userId);
+        checkEdResp.assertSuccessNeedBody();
+        //00 待提交
+        //01 审批中
+        //03 已取消
+        //22 审批退回
+        //25 额度申请被拒
+        //26 额度申请已取消
+        //27 已通过
+        switch (checkEdResp.getRetFlag()) {
+            case "01": {//审批中=已提交
+                Map<String, Object> bodyRet = new HashMap<>();
+                bodyRet.put("flag", "02");
+                bodyRet.put("token", makeToken);
+                return CommonResponse.success(bodyRet);
+            }
+            case "22": {//退回
+                Map<String, Object> bodyRet = new HashMap<>();
+                bodyRet.put("flag", "03");
+                bodyRet.put("token", makeToken);
+                return CommonResponse.success(bodyRet);
+            }
+            case "25": {//被拒绝
+                Map<String, Object> bodyRet = new HashMap<>();
+                bodyRet.put("flag", "04");
+                bodyRet.put("token", makeToken);
+                return CommonResponse.success(bodyRet);
+            }
+            case "27": {//有额度
+                Map<String, Object> bodyRet = new HashMap<>();
+                bodyRet.put("flag", "05");
+                bodyRet.put("token", makeToken);
+                return CommonResponse.success(bodyRet);
+            }
+            default:
+                return CommonResponse.fail(ConstUtil.ERROR_CODE, "额度状态错误");
+        }
+    }
+
+    public IResponse<Map> creditEntry(String authCode) throws AlipayApiException {
+        AlipayToken token = AlipayUtils.getOauthTokenByAuthCode(authCode);
+        AlipayUserInfoShareResponse alipayUserInfo = AlipayUtils.getUserInfo(token.getToken());
+        if (!Objects.equals(alipayUserInfo.getUserType(), "2"))
+            return CommonResponse.fail(ConstUtil.ERROR_CODE, "不是个人账号");
+        if (!Objects.equals(alipayUserInfo.getUserStatus(), "T"))
+            return CommonResponse.fail(ConstUtil.ERROR_CODE, "用户未认证");
+        return null;
+    }
+
+    //查询第三方账号
+    private IResponse<Map> queryUserByExternUid(String thirdUserId) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("externCompanyNo", EncryptUtil.simpleEncrypt(this.getChannelNo()));
+        map.put("externUid", EncryptUtil.simpleEncrypt(thirdUserId));
+        String url = EurekaServer.UAUTH + "/app/uauth/queryUserByExternUid";
+        return CommonRestUtils.getForMap(url, map);
+    }
+
+    //6.1.102.	(GET)额度申请校验
+    private IResponse<Map> checkEdAppl(String userId) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("channel", ConstUtil.CHANNEL);
+        params.put("channelNo", this.getChannelNo());
+        params.put("userId", userId);
+        String url = AppServerUtils.getAppServerUrl() + "/app/appserver/validate/checkEdAppl";
+        return CommonRestUtils.getForMap(url, params);
+    }
+
+    //查询个人资料
+    private IResponse<Map> queryPerCustInfo(String userId) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("channel", ConstUtil.CHANNEL);
+        params.put("channelNo", this.getChannelNo());
+        params.put("userId", userId);
+        String url = AppServerUtils.getAppServerUrl() + "/app/appserver/crm/cust/queryPerCustInfo";
+        return CommonRestUtils.getForMap(url, params);
+    }
+
 }
