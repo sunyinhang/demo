4a4fe51d42d9ee3a23c850d098b5316001a718b1
TimAndy <xuchonglei@126.com>
2018-01-27 18:18:45 +0800

    fix 初始化 bean 顺序导致的 applicationName 为空

--- haiercash-spring/src/main/java/com/haiercash/spring/trace/TraceAutoConfiguration.java
+++ haiercash-spring/src/main/java/com/haiercash/spring/trace/TraceAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.haiercash.spring.trace;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 /**
  * Created by 许崇雷 on 2018-01-27.
@@ -10,13 +11,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TraceAutoConfiguration {
     @Bean
-    @ConditionalOnMissingBean(ISequence.class)
+    @ConditionalOnMissingBean
     public ISequence sequence() {
         return new Sequence();
     }
 
     @Bean
-    @ConditionalOnMissingBean(ITracer.class)
+    @DependsOn("applicationProvider")
+    @ConditionalOnMissingBean
     public ITracer tracer(ISequence sequence) {
         return new Tracer(sequence);
     }
--- haiercash-spring/src/main/java/com/haiercash/spring/trace/Tracer.java
+++ haiercash-spring/src/main/java/com/haiercash/spring/trace/Tracer.java
@@ -1,5 +1,6 @@
 package com.haiercash.spring.trace;
 
+import com.haiercash.core.collection.ArrayUtils;
 import com.haiercash.core.collection.iterator.CharSequenceIterable;
 import com.haiercash.core.lang.CharUtils;
 import com.haiercash.core.lang.DateUtils;
@@ -21,38 +22,33 @@ public final class Tracer implements ITracer {
     private static final String TRACE_ID_SEPARATOR = Environment.Minus;
     private static final String DATE_FORMAT = "yyyyMMdd" + TRACE_ID_SEPARATOR + "HHmmss";
     private static final int APPLICATION_NAME_LENGTH = 3;
-    private static final String APPLICATION_NAME;
-    private static final String IP_LAST_BIT;
-    private static final int MIN_LENGTH_TRACE_ID = 10;
-    private static final int MAX_LENGTH_TRACE_ID = 50;
-    private static final int MIN_LENGTH_SPAN_ID = 1;
-    private static final int MAX_LENGTH_SPAN_ID = 50;
-
-    static {
-        //初始化 APPLICATION_NAME
-        String appName = ApplicationUtils.getProperties().getName();
-        APPLICATION_NAME = (appName.length() <= APPLICATION_NAME_LENGTH ? appName : appName.substring(0, APPLICATION_NAME_LENGTH)).toUpperCase();
-        //初始化 IP_LAST_BIT
-        String[] ips = HostInfo.getIpAddress();
-        if (ips.length < 1) {
-            IP_LAST_BIT = HostInfo.getHostName();
-        } else {
-            String ip = ips[0];
-            String[] bits = StringUtils.split(ip, new char[]{Environment.DotChar}, true);
-            IP_LAST_BIT = bits[bits.length - 1];
-        }
-    }
-
+    private static final int TRACE_ID_MIN_LENGTH = 10;
+    private static final int TRACE_ID_MAX_LENGTH_ = 50;
+    private static final int SPAN_ID_MIN_LENGTH = 1;
+    private static final int SPAN_ID_MAX_LENGTH = 50;
+    private final String applicationName;
+    private final String ipAddressLast;
     private final ISequence sequence;
 
     public Tracer(ISequence sequence) {
         Assert.notNull(sequence, "sequence can not be null");
+
         this.sequence = sequence;
+        String appName = ApplicationUtils.getProperties().getName();
+        this.applicationName = (appName.length() <= APPLICATION_NAME_LENGTH ? appName : appName.substring(0, APPLICATION_NAME_LENGTH)).toUpperCase();
+        String[] ipAddresses = HostInfo.getIpAddress();
+        if (ArrayUtils.isEmpty(ipAddresses)) {
+            this.ipAddressLast = HostInfo.getHostName();
+            return;
+        }
+        String ipAddress = ipAddresses[0];
+        String[] bits = StringUtils.split(ipAddress, new char[]{Environment.DotChar}, true);
+        this.ipAddressLast = bits[bits.length - 1];
     }
 
     //无效 traceId 返回 true,防止 header 注入
     private static boolean isInvalidTraceId(String traceId) {
-        if (traceId == null || traceId.length() < MIN_LENGTH_TRACE_ID || traceId.length() > MAX_LENGTH_TRACE_ID)
+        if (traceId == null || traceId.length() < TRACE_ID_MIN_LENGTH || traceId.length() > TRACE_ID_MAX_LENGTH_)
             return true;
         for (Character ch : new CharSequenceIterable(traceId)) {
             if (CharUtils.isAsciiAlphanumeric(ch)
@@ -67,7 +63,7 @@ public final class Tracer implements ITracer {
 
     //无效 spanId 返回 true,防止 header 注入
     private static boolean isInvalidSpanId(String spanId) {
-        if (spanId == null || spanId.length() < MIN_LENGTH_SPAN_ID || spanId.length() > MAX_LENGTH_SPAN_ID)
+        if (spanId == null || spanId.length() < SPAN_ID_MIN_LENGTH || spanId.length() > SPAN_ID_MAX_LENGTH)
             return true;
         for (Character ch : new CharSequenceIterable(spanId)) {
             if (CharUtils.isAsciiAlphanumeric(ch)
@@ -117,9 +113,9 @@ public final class Tracer implements ITracer {
     private String createTraceId() {
         @SuppressWarnings("StringBufferReplaceableByString")
         StringBuilder builder = new StringBuilder(50);
-        builder.append(APPLICATION_NAME)//程序名前三位
+        builder.append(this.applicationName)//程序名前三位
                 .append(TRACE_ID_SEPARATOR)
-                .append(IP_LAST_BIT)//IP 地址最后一个数字 0-255
+                .append(this.ipAddressLast)//IP 地址最后一个数字 0-255
                 .append(TRACE_ID_SEPARATOR)
                 .append(DateUtils.toString(DateUtils.now(), DATE_FORMAT))//年月日-时分秒毫秒
                 .append(TRACE_ID_SEPARATOR)
