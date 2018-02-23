9630e18c3e84a2ddeedd9f69d0cdfadb5956b3ec
TimAndy <xuchonglei@126.com>
2018-02-23 16:19:32 +0800

    支付宝添加配置项

--- payplatform-main/src/main/java/com/haiercash/payplatform/config/AlipayConfig.java
+++ payplatform-main/src/main/java/com/haiercash/payplatform/config/AlipayConfig.java
@@ -16,4 +16,7 @@ public class AlipayConfig {
     private String appPrivateKey;//app 的私钥,公钥配置到支付宝官方的管理界面 https://openhome.alipay.com/platform/detailApp.htm?appId=${appId}&tab=appDetail
     private String alipayPublicKey;//支付宝的公钥,从支付宝官方的管理界面获取
     private Integer fuwuMinScore;//生活号最小分数
+    private String wapPayReturnUrl;//支付结果回调页面,H5 提供
+    private String wapPayNotifyUrl;//订单状态通知接口,支付网关提供
+    private String subject;//支付宝 wap pay 时的 subject,商品名称
 }
