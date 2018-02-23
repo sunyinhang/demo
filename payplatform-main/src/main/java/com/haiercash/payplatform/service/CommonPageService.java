049575a1ab1f372d1c57915d9a0dcca237582852
sunyinhang <sunyinhang@bestvike.com>
2018-02-23 15:32:52 +0800

    封装 2.1.23提交还款请求 接口

--- payplatform-main/src/main/java/com/haiercash/payplatform/service/CommonPageService.java
+++ payplatform-main/src/main/java/com/haiercash/payplatform/service/CommonPageService.java
@@ -95,5 +95,12 @@ public interface CommonPageService {
      */
     Map<String, Object> getAreaCode(String provinceName, String cityName, String districtName);
 
-
+    /**
+     * 提交还款请求
+     *
+     * @param map
+     * @return
+     * @throws Exception
+     */
+    IResponse<Map> saveZdhkInfo(Map<String, Object> map);
 }
