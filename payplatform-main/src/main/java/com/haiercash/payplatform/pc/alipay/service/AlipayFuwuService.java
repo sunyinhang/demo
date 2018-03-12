bcaa0ccded542ef4ee684c7fe1fe4b9bb073d9fb
TimAndy <xuchonglei@126.com>
2018-03-12 15:10:27 +0800

    支付宝修复查询处理中返回解析错误

--- payplatform-main/src/main/java/com/haiercash/payplatform/pc/alipay/service/AlipayFuwuService.java
+++ payplatform-main/src/main/java/com/haiercash/payplatform/pc/alipay/service/AlipayFuwuService.java
@@ -285,48 +285,42 @@ public class AlipayFuwuService extends BaseService {
         String isAll = Convert.toString(params.get("isAll"));//是否全部还款
 
         //调用收单接口
-        String repayAmt = null;//总还款额
-        IResponse<Map> acqResponse;
         if ("Y".equals(isRetry)) {//处理中 isRetry = Y 查询收单处理中的订单
-            Map<String, Object> acqParams = new HashMap<>();
-            acqParams.put("sortNo", "1");//使用 applSeq 参数
-            acqParams.put("applSeq", applSeq);
-            acqParams.put("setlSts", "01");//01：还款处理中 02：还款成功 03：还款失败
-            acqParams.put("isNeedPayNo", "Y"); //需要 payNo
-            IAcqRequest request = AcqRequestBuilder.newBuilder("ACQ-2202")
-                    .sysFlag(ConstUtil.CHANNEL)
-                    .body(Collections.singletonMap("list", Collections.singletonList(acqParams)))
-                    .build();
-            acqResponse = this.acquirerClient.selectRepayRequestSetlSts(request);
-        } else {//未处理中,参数验证
-            repayAmt = Convert.toString(params.get("repayAmt"));
-            if (StringUtils.isEmpty(repayAmt))
-                throw new BusinessException(ConstUtil.ERROR_CODE, "[还款总金额]不能为空");
-            String psPerdNo = Convert.toString(params.get("psPerdNo"));
-            if (StringUtils.isEmpty(psPerdNo))
-                throw new BusinessException(ConstUtil.ERROR_CODE, "[还款期]不能为空");
-            //调用收单 还款申请
-            Map<String, Object> acqParams = new HashMap<>();
-            acqParams.put("applSeq", applSeq);
-            if ("Y".equals(isAll)) {//全部还款
-                acqParams.put("setlTyp", "01");//01：信贷还款 02：充值还款
-                acqParams.put("setlMode", "FS");//FS（全部还款）NM（归还欠款）ER（提前还款）信贷还款时必传
-            } else {//按期还款
-                acqParams.put("setlTyp", "02");//01：信贷还款 02：充值还款
-            }
-            acqParams.put("repayAmt", repayAmt);//还款总金额  repayAmt  NUMBER(16,2)  是
-            acqParams.put("psPerdNo", psPerdNo);//还款期  psPerdNo  VARCHAR2(200)  是  多个期号以“|”分隔。随借随还传“1”
-            acqParams.put("acCardNo", AlipayConfig.REPAY_APPL_CARD_NO);//还款卡号  acCardNo  VARCHAR2(30)  是
-            acqParams.put("useCoup", "N");//是否使用优惠券  useCoup  VARCHAR2(10)  是  Y：使用 N：不使用
-            acqParams.put("custNo", custNo);//客户编号  custNo  VARCHAR2(30)  是
-            acqParams.put("isNeedPayNo", "Y");//  是否需要支付流水号 isNeedPayNo	Varchar2 选填	Y--- N---否  默认为否仅支持信贷还款
-            IAcqRequest request = AcqRequestBuilder.newBuilder("ACQ-2101")
-                    .sysFlag(ConstUtil.CHANNEL)
-                    .body(Collections.singletonMap("list", Collections.singletonList(acqParams)))
-                    .build();
-            acqResponse = this.acquirerClient.saveZdhkInfo(request);
+            return this.getProcessingPay(applSeq);
         }
 
+        //未处理中,参数验证
+        return this.applyRepay(isAll, applSeq, custNo, params);
+    }
+
+    //申请还款
+    private IResponse<AlipayOrder> applyRepay(String isAll, String applSeq, String custNo, Map<String, Object> params) {
+        String repayAmt = Convert.toString(params.get("repayAmt"));
+        if (StringUtils.isEmpty(repayAmt))
+            throw new BusinessException(ConstUtil.ERROR_CODE, "[还款总金额]不能为空");
+        String psPerdNo = Convert.toString(params.get("psPerdNo"));
+        if (StringUtils.isEmpty(psPerdNo))
+            throw new BusinessException(ConstUtil.ERROR_CODE, "[还款期]不能为空");
+        //调用收单 还款申请
+        Map<String, Object> acqParams = new HashMap<>();
+        acqParams.put("applSeq", applSeq);
+        if ("Y".equals(isAll)) {//全部还款
+            acqParams.put("setlTyp", "01");//01：信贷还款 02：充值还款
+            acqParams.put("setlMode", "FS");//FS（全部还款）NM（归还欠款）ER（提前还款）信贷还款时必传
+        } else {//按期还款
+            acqParams.put("setlTyp", "02");//01：信贷还款 02：充值还款
+        }
+        acqParams.put("repayAmt", repayAmt);//还款总金额  repayAmt  NUMBER(16,2)  是
+        acqParams.put("psPerdNo", psPerdNo);//还款期  psPerdNo  VARCHAR2(200)  是  多个期号以“|”分隔。随借随还传“1”
+        acqParams.put("acCardNo", AlipayConfig.REPAY_APPL_CARD_NO);//还款卡号  acCardNo  VARCHAR2(30)  是
+        acqParams.put("useCoup", "N");//是否使用优惠券  useCoup  VARCHAR2(10)  是  Y：使用 N：不使用
+        acqParams.put("custNo", custNo);//客户编号  custNo  VARCHAR2(30)  是
+        acqParams.put("isNeedPayNo", "Y");//  是否需要支付流水号 isNeedPayNo	Varchar2 选填	Y--- N---否  默认为否仅支持信贷还款
+        IAcqRequest request = AcqRequestBuilder.newBuilder("ACQ-2101")
+                .sysFlag(ConstUtil.CHANNEL)
+                .body(Collections.singletonMap("list", Collections.singletonList(acqParams)))
+                .build();
+        IResponse<Map> acqResponse = this.acquirerClient.saveZdhkInfo(request);
         acqResponse.assertSuccessNeedBody();
         Map<String, Object> acqRespBody = acqResponse.getBody();
         if (MapUtils.isEmpty(acqRespBody)) {
@@ -368,6 +362,78 @@ public class AlipayFuwuService extends BaseService {
                 this.logger.info("收单 ACQ-2202 未返回 repayAmt");
                 throw new BusinessException(ConstUtil.ERROR_CODE, failReason);
             }
+        }
+
+        //返回
+        AlipayOrder order = new AlipayOrder();
+        order.setApplSeq(applSeq);
+        order.setPayNo(payNo);
+        order.setRepaySeq(repaySeq);
+        order.setRepayAmt(repayAmt);
+        return CommonResponse.success(order);
+    }
+
+    //获取处理中还款
+    private IResponse<AlipayOrder> getProcessingPay(String applSeq) {
+        Map<String, Object> acqParams = new HashMap<>();
+        acqParams.put("sortNo", "1");//使用 applSeq 参数
+        acqParams.put("applSeq", applSeq);
+        acqParams.put("setlSts", "01");//01：还款处理中 02：还款成功 03：还款失败
+        acqParams.put("isNeedPayNo", "Y"); //需要 payNo
+        IAcqRequest request = AcqRequestBuilder.newBuilder("ACQ-2202")
+                .sysFlag(ConstUtil.CHANNEL)
+                .body(Collections.singletonMap("list", Collections.singletonList(acqParams)))
+                .build();
+        IResponse<Map> acqResponse = this.acquirerClient.selectRepayRequestSetlSts(request);
+        acqResponse.assertSuccessNeedBody();
+        Map<String, Object> acqRespBody = acqResponse.getBody();
+        if (MapUtils.isEmpty(acqRespBody)) {
+            this.logger.info("收单提交还款请求返回的 body 为空");
+            throw new BusinessException(ConstUtil.ERROR_CODE, ConstUtil.TIME_OUT);
+        }
+        List<Map> list = (List<Map>) acqRespBody.get("list");
+        if (CollectionUtils.isEmpty(list)) {
+            this.logger.info("收单提交还款请求返回的 body 的 list 为空");
+            throw new BusinessException(ConstUtil.ERROR_CODE, "申请还款失败");
+        }
+        Map<String, Object> listOne = list.get(0);
+        if (MapUtils.isEmpty(listOne)) {
+            this.logger.info("收单提交还款请求返回的 body 的 list 的第一个元素内容为空");
+            throw new BusinessException(ConstUtil.ERROR_CODE, "申请还款失败");
+        }
+        List<Object> rePayRecord = (List<Object>) listOne.get("rePayRecord");
+        if (CollectionUtils.isEmpty(rePayRecord)) {
+            this.logger.info("收单提交还款请求返回的 rePayRecord 为空");
+            throw new BusinessException(ConstUtil.ERROR_CODE, "申请还款失败");
+        }
+        Map<String, Object> one = (Map<String, Object>) rePayRecord.get(0);
+        if (MapUtils.isEmpty(one)) {
+            this.logger.info("收单提交还款请求返回的 rePayRecord 的第一个元素内容为空");
+            throw new BusinessException(ConstUtil.ERROR_CODE, "申请还款失败");
+        }
+        String failReason = Convert.defaultString(one.get("failReason"), "申请还款失败");//失败原因
+        String setlSts = Convert.toString(one.get("setlSts"));//01 处理中
+        if (!"01".equals(setlSts)) {
+            this.logger.info("收单返回 setlSts:" + setlSts);
+            throw new BusinessException(ConstUtil.ERROR_CODE, failReason);
+        }
+        String payNo = Convert.toString(one.get("payNo"));
+        this.logger.info("收单返回 payNo:" + payNo);
+        if (StringUtils.isEmpty(payNo)) {
+            this.logger.info("收单未返回 payNo");
+            throw new BusinessException(ConstUtil.ERROR_CODE, failReason);
+        }
+        String repaySeq = Convert.toString(one.get("repaySeq"));
+        this.logger.info("收单返回 repaySeq:" + repaySeq);
+        if (StringUtils.isEmpty(repaySeq)) {
+            this.logger.info("收单未返回 repaySeq");
+            throw new BusinessException(ConstUtil.ERROR_CODE, failReason);
+        }
+        //repayAmt 为空说明 有处理中的
+        String repayAmt = Convert.toString(one.get("repayAmt"));
+        if (StringUtils.isEmpty(repayAmt)) {
+            this.logger.info("收单 ACQ-2202 未返回 repayAmt");
+            throw new BusinessException(ConstUtil.ERROR_CODE, failReason);
         }
 
         //返回
