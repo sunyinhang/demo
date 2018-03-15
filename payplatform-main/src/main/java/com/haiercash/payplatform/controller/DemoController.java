c17db9c8785feb7e27e8d318a906ace5557ef765
TimAndy <xuchonglei@126.com>
2018-03-15 16:05:06 +0800

    超时测试接口支持 GET 和 POST

--- payplatform-main/src/main/java/com/haiercash/payplatform/controller/DemoController.java
+++ payplatform-main/src/main/java/com/haiercash/payplatform/controller/DemoController.java
@@ -9,7 +9,8 @@ import com.haiercash.spring.controller.BaseController;
 import com.haiercash.spring.rest.IResponse;
 import com.haiercash.spring.rest.common.CommonRestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +31,7 @@ public class DemoController extends BaseController {
         super("01");
     }
 
-    @PostMapping("/api/payment/timeout/test")
+    @RequestMapping(value = "/api/payment/timeout/test", method = {RequestMethod.GET, RequestMethod.POST})
     public void testTimeout(@RequestParam(name = "value", required = false) String value, HttpServletResponse response) throws IOException {
         int millis = Convert.defaultInteger(value);
         ThreadUtils.sleep(millis);
